86473bf40b2ac821de60d4313c6e38b1827f5119
Salmanul Farzy <salmanulfarzy@gmail.com>
2018-05-20 14:23:43 +0000

    Deploy salmanulfarzy/spaceship-prompt to github.com/salmanulfarzy/spaceship-prompt.git:gh-pages

--- sections/package.zsh
+++ sections/package.zsh
@@ -27,7 +27,7 @@ spaceship_package() {
   [[ -f package.json ]] || return
 
   # Grep and cut out package version
-  local package_version=$(grep -E '"version": "v?([0-9]+\.){1,}' package.json | cut -d\" -f4 2> /dev/null)
+  local package_version=$(grep -E '^  "version": "v?([0-9]+\.){1,}' package.json | cut -d\" -f4  2>/dev/null)
 
   # Handle version not found
   if [ ! "$package_version" ]; then
