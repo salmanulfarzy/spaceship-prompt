e803718f3734566e0146ebbfae714d09ede66218
Cole Helbling <cole.e.helbling@gmail.com>
2018-08-08 12:27:31 -0700

    Add async RPROMPT support

--- spaceship.zsh
+++ spaceship.zsh
@@ -143,15 +143,6 @@ spaceship::deprecated SPACESHIP_PYENV_COLOR "Use %bSPACESHIP_PYTHON_COLOR%b inst
 # An entry point of prompt
 # ------------------------------------------------------------------------------
 
-# $RPROMPT
-# Optional (right) prompt
-spaceship_rprompt() {
-  # Retrieve exit code of last command to use in exit_code
-  RETVAL=$?
-
-  spaceship::compose_prompt $SPACESHIP_RPROMPT_ORDER
-}
-
 # PS2
 # Continuation interactive prompt
 spaceship_ps2() {
@@ -181,6 +172,7 @@ spaceship_async_callback() {
 
   SPACESHIP_ASYNC_NEED_REDRAW_PROMPT=0
   PROMPT=$(spaceship::compose_prompt $SPACESHIP_PROMPT_ORDER)
+  RPROMPT=$(spaceship::compose_prompt $SPACESHIP_RPROMPT_ORDER)
   zle .reset-prompt
   zle -R
 }
@@ -214,35 +206,6 @@ prompt_spaceship_setup() {
 
   # Expose Spaceship to environment variables
   PS2='$(spaceship_ps2)'
-  RPS1='$(spaceship_rprompt)'
-
-  # Store prompt expansion symbols for in-place expansion via (%). For
-  # some reason it does not work without storing them in a variable first.
-  typeset -ga prompt_spaceship_debug_depth
-  prompt_spaceship_debug_depth=('%e' '%N' '%x')
-
-  # Compare is used to check if %N equals %x. When they differ, the main
-  # prompt is used to allow displaying both file name and function. When
-  # they match, we use the secondary prompt to avoid displaying duplicate
-  # information.
-  local -A ps4_parts
-  ps4_parts=(
-    depth     '%F{yellow}${(l:${(%)prompt_spaceship_debug_depth[1]}::+:)}%f'
-    compare   '${${(%)prompt_spaceship_debug_depth[2]}:#${(%)prompt_spaceship_debug_depth[3]}}'
-    main      '%F{blue}${${(%)prompt_spaceship_debug_depth[3]}:t}%f%F{242}:%I%f %F{242}@%f%F{blue}%N%f%F{242}:%i%f'
-    secondary '%F{blue}%N%f%F{242}:%i'
-    prompt    '%F{242}>%f '
-  )
-
-  # Combine the parts with conditional logic. First the `:+` operator is
-  # used to replace `compare` either with `main` or an ampty string. Then
-  # the `:-` operator is used so that if `compare` becomes an empty
-  # string, it is replaced with `secondary`.
-  local ps4_symbols='${${'${ps4_parts[compare]}':+"'${ps4_parts[main]}'"}:-"'${ps4_parts[secondary]}'"}'
-
-  # Improve the debug prompt (PS4), show depth by repeating the +-sign and
-  # add colors to highlight essential parts like file and function name.
-  PROMPT4="${ps4_parts[depth]} ${ps4_symbols}${ps4_parts[prompt]}"
 }
 
 # ------------------------------------------------------------------------------
