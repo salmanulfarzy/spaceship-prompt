737b808b34646bbbc54aabed15807ccabb89924e
Salmanul Farzy <salmanulfarzy@gmail.com>
2018-12-14 11:22:35 +0530

    Exit package section if 'null'

--- sections/package.zsh
+++ sections/package.zsh
@@ -36,7 +36,7 @@ spaceship_package() {
     package_version=$(node -p "require('./package.json').version" 2> /dev/null)
   fi
 
-  [[ -z $package_version || "$package_version" == "undefined" ]] && return
+  [[ -z $package_version || "$package_version" == "undefined"  || "$package_version" == "null" ]] && return
 
   spaceship::section \
     "$SPACESHIP_PACKAGE_COLOR" \
