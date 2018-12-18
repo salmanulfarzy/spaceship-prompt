ff76cf04329146ea620b35c09e44e52a11e04d96
Salmanul Farzy <salmanulfarzy@gmail.com>
2018-12-18 20:34:51 +0530

    Make python section async

--- sections/python.zsh
+++ sections/python.zsh
@@ -18,12 +18,16 @@ SPACESHIP_PYTHON_COLOR="${SPACESHIP_PYTHON_COLOR="yellow"}"
 # Section
 # ------------------------------------------------------------------------------
 
-# Show current version of python
-spaceship_python() {
+spaceship_async_job_load_python() {
   [[ $SPACESHIP_PYTHON_SHOW == false ]] && return
 
   # Show python python version only for Python-specific folders
   [[ -f requirements.txt || -f Pipfile || -n *.py(#qN^/) ]] || return
+
+  async_job spaceship spaceship_async_job_python
+}
+
+spaceship_async_job_python() {
 
   local 'py_version'
 
@@ -31,11 +35,18 @@ spaceship_python() {
     py_version=${(@)$(python -V 2>&1)[2]}
   fi
 
-  [[ -z $py_version ]] && return
+  echo $py_version
+}
+
+# Show current version of python
+spaceship_python() {
+  local async_result="${SPACESHIP_ASYNC_RESULTS[spaceship_async_job_python]}"
+
+  [[ -z $async_result ]] && return
 
   spaceship::section \
     "$SPACESHIP_PYTHON_COLOR" \
     "$SPACESHIP_PYTHON_PREFIX" \
-    "${SPACESHIP_PYTHON_SYMBOL}${py_version}" \
+    "${SPACESHIP_PYTHON_SYMBOL}${async_result}" \
     "$SPACESHIP_PYTHON_SUFFIX"
 }
